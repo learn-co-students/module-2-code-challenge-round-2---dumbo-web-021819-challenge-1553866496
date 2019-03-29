eaee7597e56fd5d9b8404546310328383b2d7adb
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-03-29 11:20:50 -0400

    Done

---
+++ db/migrate/20190329140022_create_appearences.rb
@@ -0,0 +1,11 @@
+class CreateAppearences < ActiveRecord::Migration[5.1]
+  def change
+    create_table :appearences do |t|
+      t.belongs_to :guest, foreign_key: true
+      t.belongs_to :episode, foreign_key: true
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
